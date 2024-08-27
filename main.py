4b62d604af8ae429f5578649d36649c3645e81ab
woolwood <35411775+woolwood@users.noreply.github.com>
2024-08-28 01:30:16 +0200

    Add game clock.

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    game_clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -12,6 +14,7 @@ def main():
                 return
         screen.fill(000000)
         pygame.display.flip()
+        dt = game_clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
