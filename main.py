d1ab3b090b5ae132007451992a7d09180e28b285
woolwood <35411775+woolwood@users.noreply.github.com>
2024-08-28 02:17:41 +0200

    Add rotation control keys.

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill(000000)
+        player.update(dt)
         player.draw(screen)
         pygame.display.flip()
         dt = game_clock.tick(60) / 1000
