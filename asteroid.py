dc1dc222c057f471710375e79e1fbc3d12e23f69
woolwood <35411775+woolwood@users.noreply.github.com>
2024-08-28 22:17:36 +0200

    Add asteroids.

---
+++ asteroid.py
@@ -0,0 +1,15 @@
+from constants import *
+from circleshape import *
+
+
+class Asteroid(CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+        self.position = pygame.Vector2(x, y)
+        self.radius = radius
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, "white", self.position, self.radius, width=2)
+
+    def update(self, dt):
+        self.position += self.velocity * dt
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from constants import *
 from player import *
+from asteroid import *
+from asteroidfield import *
 
 
 def main():
@@ -10,15 +12,30 @@ def main():
     dt = 0
     X = SCREEN_WIDTH / 2
     Y = SCREEN_HEIGHT / 2
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
     player = Player(X, Y)
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+
+    asteroid_field = AsteroidField()
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(000000)
-        player.update(dt)
-        player.draw(screen)
+
+        for p in updatable:
+            p.update(dt)
+
+        for p in drawable:
+            p.draw(screen)
         pygame.display.flip()
         dt = game_clock.tick(60) / 1000
 
